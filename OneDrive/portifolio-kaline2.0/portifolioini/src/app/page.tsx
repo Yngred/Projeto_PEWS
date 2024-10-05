ec6f7a70aa4ec576913003a8d568b92b03830566
Yngred <yndikaline4@gmail.com>
2024-10-04 23:35:47 -0300

    nome do commit

---
+++ OneDrive/portifolio-kaline2.0/portifolioini/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-blue-100">
+      <h1 className="text-4xl font-bold mb-4 text-center">Olá, sou a Yngred</h1>
+      <p className="text-lg text-center">
+        Sou desenvolvedora backend com foco em criar soluções eficientes e bem estruturadas. 
+      </p>
+    </div>
+  );
+}
+
